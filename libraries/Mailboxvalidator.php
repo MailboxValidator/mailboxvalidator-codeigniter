8410ae49440b866701e4a6f124c83c2362ac49c8
MailboxValidator <support@mailboxvalidator.com>
2020-10-26 09:43:39 +0800

    Updated Mailboxvalidator.php

--- libraries/Mailboxvalidator.php
+++ libraries/Mailboxvalidator.php
@@ -1,7 +1,7 @@
 <?php
 (defined('BASEPATH') || defined('SYSPATH')) or die('No direct access allowed.');
 
-require_once('MailboxValidator/SingleValidation.php');
+require_once('MailboxValidator/EmailValidation.php');
 
 class Mailboxvalidator {
 
@@ -12,20 +12,20 @@ class Mailboxvalidator {
 
 		if ($params != '') {
 			$key = $params['mbv_api_key'];
-			$this->mbv = new \MailboxValidator\SingleValidation($key);
+			$this->mbv = new \MailboxValidator\EmailValidation($key);
 		} else {
-			$this->mbv = new \MailboxValidator\SingleValidation($CI->config->item('mbv_api_key'));
+			$this->mbv = new \MailboxValidator\EmailValidation($CI->config->item('mbv_api_key'));
 		}
 		log_message('debug', "Mailboxvalidator Class Initialized.");
 	}
 
 	public function get_single_result($email) {
 		if ($email != ''){
-			return $this->mbv->ValidateEmail($email);
+			return $this->mbv->validateEmail($email);
 		}
 	}
 	
-	public function is_email_free($email) {
+	public function isFreeEmail($email) {
 		if ($email != ''){
 			$result = $this->mbv->FreeEmail($email);
 			if ($result != false && $result->error_code == '') {
@@ -45,7 +45,7 @@ class Mailboxvalidator {
 
 	public function is_email_disposable($email) {
 		if ($email != ''){
-			$result = $this->mbv->DisposableEmail($email);
+			$result = $this->mbv->isDisposableEmail($email);
 			if ($result != false && $result->error_code == '') {
 				if ($result->is_disposable == 'True') {
 					return true;
